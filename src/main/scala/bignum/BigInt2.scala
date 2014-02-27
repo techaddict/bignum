131f4fa17d01e9c9827f42c8ad2bbad26182321c
Sandeep <sandeep@techaddict.me>
2014-02-27 17:18:26 +0530

    Minor changes to BigInt2

--- src/main/scala/bignum/BigInt2.scala
+++ src/main/scala/bignum/BigInt2.scala
@@ -189,7 +189,7 @@ object BigInt2 {
 
   def apply(a: String) = new BigInt2(a, 10)
   def apply(a: String, radix: Int) = new BigInt2(a, radix)
-  def apply(sign: Int, value: Int) = new BigInt2(sign, Array(value))
+  private[bignum] def apply(sign: Int, value: Int) = new BigInt2(sign, Array(value))
   private[bignum] def apply(sign: Int, value: Array[Int]) = new BigInt2(sign, value)
   def apply(a: Int) = valueOf(a)
   def apply(a: Long) = valueOf(a)
@@ -209,7 +209,7 @@ object BigInt2 {
       ret
     }
 
-  @inline private[bignum] def valueOf(a: Long): BigInt2 =
+  private[bignum] def valueOf(a: Long): BigInt2 =
     if (a == Long.MinValue)
       BigInt2("-9223372036854775808")
     else if (a < 0)
@@ -221,7 +221,7 @@ object BigInt2 {
       else BigInt2(a.signum, Array[Int](Lo, Hi))
     }
 
-  @inline private[bignum] def valueOf(a: Int): BigInt2 =
+  private[bignum] def valueOf(a: Int): BigInt2 =
     if (a < 0) {
       if (a != -1) BigInt2(-1, -a)
       else minusOne
