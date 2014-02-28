258eb69eef1ea9da15fcdc15ff1f220571ddc3df
Sandeep <sandeep@techaddict.me>
2014-02-28 19:01:33 +0530

    Very minor Changes

--- benchmark/src/main/scala/bignum/Runner.scala
+++ benchmark/src/main/scala/bignum/Runner.scala
@@ -6,7 +6,7 @@ object Runner {
 
   def main(args: Array[String]) {
     CaliperRunner.main(classOf[Add], args: _*)
-    CaliperRunner.main(classOf[Multiply], args: _*)
+  //  CaliperRunner.main(classOf[Multiply], args: _*)
   }
 
 }
